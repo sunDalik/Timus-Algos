d8e910f0f994c559da5f837bfec9b9cd30b2cdbb
sunDalik <dali1999@mail.ru>
2019-05-30 17:26:32 +0300

    okay algorithmus

---
+++ 1160.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+struct edge {
+    int a, b, l;
+};
+
+bool compare(edge a, edge b) {
+    return a.l < b.l;
+}
+
+int find(int x, int *parents) {
+    if (parents[x] != x) parents[x] = find(parents[x], parents);
+    return parents[x];
+}
+
+int main() {
+    int n, m, max_l = 0;
+    cin >> n >> m;
+    int parents[n];
+    vector<edge> connections;
+    for (int i = 0; i < m; ++i) {
+        int a, b, l;
+        cin >> a >> b >> l;
+        connections.push_back({a - 1, b - 1, l});
+    }
+
+    sort(connections.begin(), connections.end(), compare);
+
+    int conn_counter = 0;
+    for (int i = 0; i < n; i++) parents[i] = i;
+    for (int i = 0; i < m; ++i) {
+        int x = find(connections[i].a, parents);
+        int y = find(connections[i].b, parents);
+        if (x != y) {
+            if (connections[i].l > max_l) max_l = connections[i].l;
+            connections[i].l = -1;
+            parents[y] = x;
+            conn_counter++;
+            if (conn_counter == n - 1) break;
+        }
+    }
+
+    cout << max_l << "\n" << n - 1 << "\n";
+    for (int i = 0; i < m; ++i) {
+        if (connections[i].l == -1) {
+            cout << connections[i].a + 1 << " " << connections[i].b + 1 << "\n";
+        }
+    }
+    return 0;
+}
